341bd40b0f473884a405c25093cd16c71f6b84e1
derekdyer0309 <derekdyer02@gmail.com>
2020-02-05 10:46:56 -0800

    created pickup with dummy data for basic layout, need to hit API to actually see what these things will be called

--- replate/src/components/Pickup.js
+++ replate/src/components/Pickup.js
@@ -1,6 +1,11 @@
 import React, { useContext } from 'react';
 
-export const Pickup = () => {
-
-    
+export const Pickup = props => {
+    return (
+        <div className="pickup">
+            <h4 className="pickup-name">{props.pickup.name}</h4>
+            <p className="pickup-location">{props.pickup.location}</p>
+    <p className="pickup-business">{props.pickup.business}</p>
+        </div>
+    );
 }
