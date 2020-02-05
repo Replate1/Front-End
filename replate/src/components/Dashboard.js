e4d925f56a8dfdec6500b8dc63e3507318c801f6
mastaryan <sizemore.ryan@gmail.com>
2020-02-05 15:42:42 -0500

    Dashboard

--- replate/src/components/Dashboard.js
+++ replate/src/components/Dashboard.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ModelContext } from "../Context/ModelContext";
 
-const Dashboard = () => {
+// const Dashboard = () => {
+//   return (
+//   <div>
+//       <h1>Dashboard</h1>
+//   </div>
+  
+//   )
+// };
+
+// export default Dashboard;
+
+// Dashboard code start
+
+const Dashboard = props => {
+const [names, setNames] = useState([
+    {
+      id: 0,
+      name: "Deepu"
+    },
+    {
+      id: 1,
+      name: "Chintu"
+    },
+    {
+      id: 2,
+      name: "Chaman"
+    }
+  ]);
+
+  const openModel = () => setCurrentModel({ name: "DashboardModal" });
+
+  const handleNameDelete = id =>
+    setCurrentModel({
+      name: "ConfirmModal",
+      props: {
+        title: "Confirm Delete",
+        cb: () => {
+          setNames(names.filter(n => n.id !== id));
+          setCurrentModel(null);
+        }
+      }
+    });
+
   return (
-  <div>
-      <h1>Dashboard</h1>
-  </div>
-  )
+    <div>
+      <p>Dashboard</p>
+      <button onClick={openModel}>Open Modal</button>
+      <hr />
+      {names.map(n => (
+        <p key={n.id}>
+          {n.name} - <span onClick={() => handleNameDelete(n.id)}>DELETE</span>
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default Dashboard;
